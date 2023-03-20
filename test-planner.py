d7ba4af581d3b729bd85b0658100ec96b48f0467
AdwaitJadhav <103038732+AdwaitJadhav@users.noreply.github.com>
2023-03-20 22:19:14 +0530

    Update test-planner.py

--- test-planner.py
+++ test-planner.py
@@ -17,7 +17,8 @@ db=mysql.connector.connect(
 cursor=db.cursor()
 create_table="CREATE TABLE IF NOT EXISTS question_bank(id int, question varchar(255), co varchar(255), marks int, PRIMARY KEY(id))"
 cursor.execute(create_table)
-dataf =pandas.read_excel('questions.xlsx')
+
+dataf =pandas.read_excel('question.xlsx')
 row=len(dataf)
 column=len(dataf.columns)
 data=dataf.values.tolist()
@@ -27,18 +28,21 @@ for i in range(0,row):
     co=data[i][2]
     marks=data[i][3]
     #print(question)
-    cursor.execute(f"INSERT INTO question_bank VALUES ({qno},'{question}',{co},{marks})")
+    cursor.execute(f"INSERT INTO question_bank VALUES ({qno},'{question}','{co}',{marks})")
     db.commit()
 cursor.execute("ALTER TABLE question_bank ORDER BY co")
 co_list=dataf.co.unique()
 no_co=len(co_list)
+qlist=[]
+for i in range(0,no_co):
+    no_of_questions=int(input(f"Enter number of questions to get from co{co_list[i]}: "))
+    qlist.append(no_of_questions)
+print("Question\tMarks")
 for i in range(0,no_co):
     cursor.execute(f"SELECT question, marks FROM question_bank WHERE co={co_list[i]}")
     result=cursor.fetchall()
-    for row in result:
-        print(row)
+    question_display=random.sample(result,qlist[i])
+    for i in question_display:
+        print(i)
         print("\n")
 cursor.execute("DROP TABLE question_bank")
-
-
-
